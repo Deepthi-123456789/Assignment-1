c3896c7df61f062d802a05acddbe781acce7bd4d
Deepthi <jonnalagaddadeepthi96@gmail.com>
2024-11-04 13:19:48 +0530

    mesg

---
+++ frontend/pages/contact.js
@@ -0,0 +1,6 @@
+// frontend/pages/contact.js
+const contact = () => {
+    return <h1>contact</h1>;
+};
+
+export default contact;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,1 +1,13 @@
+// frontend/pages/index.js
 export const backendApiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Next.js!</h1>
+            <p>API Base URL: {backendApiBaseUrl}</p>
+        </div>
+    );
+};
+
+export default Home;
